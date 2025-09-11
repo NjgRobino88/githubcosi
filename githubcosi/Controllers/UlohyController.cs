4c0ad5ae90e9ee8b17e139f9d99bbf852bce665c
NjgRobino88 <tomantv810@gmail.com>
2025-09-11 11:04:45 +0200

    Niove ulohy

---
+++ githubcosi/Controllers/UlohyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace githubcosi.Controllers
+{
+    public class UlohyController : Controller
+    {
+        public IActionResult Uloha1()
+        {
+            return View();
+        }
+    }
+}
